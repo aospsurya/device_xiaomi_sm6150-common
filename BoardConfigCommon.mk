b46bf65039b4996d51fa92e4f5c6cbb886ca0e86
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-04-12 02:08:04 +0530

    sm6150-common: tmp kernel stuff
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -94,7 +94,6 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
     HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 
